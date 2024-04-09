9c01d0cde3b190e450fb1c1786fdf7a95a771444
Enrique Iranzo Martínez <eiranzo@futurespace.es>
2024-04-09 12:53:49 +0200

    Tue Apr  9 12:53:49     2024

---
+++ Modulos/Spring/hellospringboot/hellospringboot/src/test/java/com/practica/hellospringboot/HellospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.hellospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
