cafb4179f19b37c13ceda605698d3e19ca1984bb
Enrique Iranzo Martínez <eiranzo@futurespace.es>
2024-04-10 13:15:50 +0200

    `date`

--- Modulos/Spring/mobile-app-ws-quick-start-master/src/main/java/mobileappwsquickstartmaster/app/ws/model/request/UserDetailsRequestModel.java
+++ Modulos/Spring/mobile-app-ws-quick-start-master/src/main/java/mobileappwsquickstartmaster/app/ws/model/request/UserDetailsRequestModel.java
@@ -1,9 +1,20 @@
 package mobileappwsquickstartmaster.app.ws.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class UserDetailsRequestModel {
+    @NotNull (message = "First name cannot be null")
     private String firstName;
+    @NotNull (message = "Last name cannot be null")
+
     private String lastName;
+    @NotNull (message = "Email name cannot be null")
+    @Email
     private String email;
+    @NotNull (message = "Password name cannot be null")
+    @Size(min=8,max=16,message="Password must be equal or greater than 8 characters and less than 16")
     private String password;
 
     public String getFirstName() {
