f110d047409033092d36927ed1339d2a9f38eadd
Enrique Iranzo Martínez <eiranzo@futurespace.es>
2024-04-11 13:27:14 +0200

    11/04/2024 13:27:14,22

---
+++ Ejercicios/Spring/ejercicios/src/main/java/com/ejercicios/app/ws/userservice/UserService.java
@@ -0,0 +1,9 @@
+package com.ejercicios.app.ws.userservice;
+
+import com.ejercicios.app.ws.model.request.PersonDetailsRequestModel;
+
+public interface UserService {
+    void createPerson (PersonDetailsRequestModel personDetails){
+
+    }
+}
