6e9cd9e4c90447a1ef56746f5876f024ad21300b
Enrique Iranzo Martínez <eiranzo@futurespace.es>
2024-04-17 11:18:00 +0200

    17/04/2024 11:18:00,36

---
+++ Ejercicios/Spring Data JPA/ejercicios/src/main/java/com/ejerciciosJPA/EjerciciosJpaApplication.java
@@ -0,0 +1,13 @@
+package com.ejerciciosJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciosJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciosJpaApplication.class, args);
+	}
+
+}
---
+++ Ejercicios/Spring Data JPA/ejercicios/src/test/java/com/ejerciciosJPA/EjerciciosJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejerciciosJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciosJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
