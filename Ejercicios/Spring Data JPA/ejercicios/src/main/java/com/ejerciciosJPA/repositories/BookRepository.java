f87b5ea2cd511eb6844d6a6cefe45f74eea2885a
Enrique Iranzo Martínez <eiranzo@futurespace.es>
2024-04-19 13:59:50 +0200

    19/04/2024 13:59:50,87

--- Ejercicios/Spring Data JPA/ejercicios/src/main/java/com/ejerciciosJPA/repositories/BookRepository.java
+++ Ejercicios/Spring Data JPA/ejercicios/src/main/java/com/ejerciciosJPA/repositories/BookRepository.java
@@ -11,16 +11,20 @@ import java.util.List;
 @Repository
 public interface BookRepository extends JpaRepository<Book,Long> {
     //Con consulta @Query
+    //Yo es que solo había tenido en cuenta el año de publicación, lo tengo guardado como un Int, no como Date, por tanto lo hago de mayores de 2001
     @Query("SELECT b FROM Book b WHERE b.publicationDate > 2001")
     List<Book> findBooksAfter(int year);
 
     //Con método
     List<Book>findByPublicationDateGreaterThan(int year);
+    //Encontrar por fecha de publicación
     List<Book>findByPublicationDate(int year);
+    //Encontrar por Isbn
     List<Book>findByIsbn(int isbn);
+    // Encontrar por editorial
     @Query ("SELECT b FROM Book b JOIN b.publisher p WHERE p.name = ?1")
     List<Book>findByPublisher(String name);
-
+    //Encontrar por editorial y fecha de publicación
     @Query("SELECT b FROM Book b JOIN b.publisher p WHERE p.name = ?1 AND b.publicationDate = ?2")
     List<Book>findByDateAndPublisher(String name, int year);
 
