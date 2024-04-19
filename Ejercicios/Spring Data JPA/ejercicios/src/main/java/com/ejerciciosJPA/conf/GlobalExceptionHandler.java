6e006f5cbaf236f6bad0776f1ff2dc96ebc0124f
Enrique Iranzo Martínez <eiranzo@futurespace.es>
2024-04-19 13:51:55 +0200

    19/04/2024 13:51:55,76

--- Ejercicios/Spring Data JPA/ejercicios/src/main/java/com/ejerciciosJPA/conf/GlobalExceptionHandler.java
+++ Ejercicios/Spring Data JPA/ejercicios/src/main/java/com/ejerciciosJPA/conf/GlobalExceptionHandler.java
@@ -1,16 +1,26 @@
 package com.ejerciciosJPA.conf;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<String> handleResponseStatusEx(ResponseStatusException ex){
         HttpStatusCode status = ex.getStatusCode();
-        return ResponseEntity.status(status).body("Hola");
+        String message = ex.getReason();
+        if (status == HttpStatus.NOT_FOUND){
+            message = "No se encuentra la ruta solicitada";
+        }
+        else if (status == HttpStatus.NO_CONTENT){
+            message = "No hay coincidencias con la consulta";
+        }
+        return new ResponseEntity<>(message,new HttpHeaders(),status);
     }
 }
