6001bc3d41bf5884a018c56fee54dd981829aaf0
Enrique Iranzo Martínez <eiranzo@futurespace.es>
2024-04-25 11:24:53 +0200

    25/04/2024 11:24:52,98

---
+++ Ejercicios/Vue/ejercicios/src/components/CharacterCard.vue
@@ -0,0 +1,65 @@
+<template>
+    <transition name="fade">
+    <v-card
+          class="mx-auto"
+          max-width="344"
+          elevation="10"
+          :color="color.house"
+        >
+          <v-img
+            height="450px"
+            :src="image"
+            cover
+          ></v-img>
+
+          <v-card-title class="title">
+            {{name}}
+          </v-card-title>
+
+          <v-card-subtitle class="subtitle">
+            <ul>
+              <li>Especie: {{ species }}</li>
+              <li>Casa : {{ house }}</li>
+              <li>Fecha de nacimiento : {{ dateOfBirth }}</li>
+              <li>Actor / actriz : {{ actor }}</li>
+            </ul>
+          </v-card-subtitle>
+    </v-card>
+    </transition>
+</template>
+<script>
+    export default{
+        name: 'CharacterCard',
+        data:()=>({
+            color: {
+                "Gryffindor":"red-darken-4",
+                "Slytherin":"teal-darken-3",
+                "Hufflepuff":"yellow-darken-1",
+                "Ravenclaw":"indigo-accent-1"
+            }
+        }),
+        props:{
+            image:String,
+            name:String,
+            species:String,
+            house:String,
+            dateOfBirth:String,
+            actor:String
+        }
+    }
+</script>
+<style scoped>
+    .title{
+        font-size: 24px;
+        font-weight: bold;
+    }
+    .subtitle{
+        font-size: 16px;
+    }
+    .fade-enter-active, .fade-leave-active{
+        transition: opacity 0.5s;
+    }
+    .fade-enter, .fade-leave-ro{
+        opacity: 0;
+    }
+</style>
--- Ejercicios/Vue/ejercicios/src/views/ExerciseTwoView.vue
+++ Ejercicios/Vue/ejercicios/src/views/ExerciseTwoView.vue
@@ -4,42 +4,23 @@
       <div v-if="error" v-text="error"></div>
       <v-row class="mt-2">
         <v-col cols="4" v-for="character of characters" :key="character.id">
-          <v-card
-          class="mx-auto"
-          max-width="344"
-        >
-          <v-img
-            height="450px"
-            :src="character.image"
-            cover
-          ></v-img>
-
-          <v-card-title>
-            {{character.name}}
-          </v-card-title>
-
-          <v-card-subtitle>
-            <ul>
-              <li>Especie: {{ character.species }}</li>
-              <li>Casa : {{ character.house }}</li>
-              <li>Fecha de nacimiento : {{ character.dateOfBirth }}</li>
-              <li>Actor / actriz : {{ character.actor }}</li>
-            </ul>
-          </v-card-subtitle>
-        </v-card>
+          <CharacterCard :actor="character.actor" :date-of-birth="character.dateOfBirth" :house="character.house"
+            :name="character.name" :species="character.species" :image="character.image"/>
         </v-col>
       </v-row>
       </div>
 </template>
 
 <script>
+import CharacterCard from '@/components/CharacterCard.vue';
+
 
   export default {
     name: 'ExerciseTwoView',
 
     components: {
-    
-    },
+    CharacterCard
+},
     computed: {
       characters() {
         return this.$store.state.characters;
