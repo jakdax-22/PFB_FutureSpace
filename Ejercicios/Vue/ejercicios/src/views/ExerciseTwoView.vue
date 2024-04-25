358885330a71b4b34f6076350c3486d8980bc19d
Enrique Iranzo Martínez <eiranzo@futurespace.es>
2024-04-25 10:33:56 +0200

    25/04/2024 10:33:55,97

--- Ejercicios/Vue/ejercicios/src/views/ExerciseTwoView.vue
+++ Ejercicios/Vue/ejercicios/src/views/ExerciseTwoView.vue
@@ -2,10 +2,34 @@
     <div>
       <div v-show="loading">Cargando...</div>
       <div v-if="error" v-text="error"></div>
-      <div v-for="character of characters" :key="character.id">
-        <p>{{ character.name }}</p>
+      <v-row class="mt-2">
+        <v-col cols="4" v-for="character of characters" :key="character.id">
+          <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            height="450px"
+            :src="character.image"
+            cover
+          ></v-img>
+
+          <v-card-title>
+            {{character.name}}
+          </v-card-title>
+
+          <v-card-subtitle>
+            <ul>
+              <li>Especie: {{ character.species }}</li>
+              <li>Casa : {{ character.house }}</li>
+              <li>Fecha de nacimiento : {{ character.dateOfBirth }}</li>
+              <li>Actor / actriz : {{ character.actor }}</li>
+            </ul>
+          </v-card-subtitle>
+        </v-card>
+        </v-col>
+      </v-row>
       </div>
-    </div>
 </template>
 
 <script>
@@ -21,13 +45,13 @@
         return this.$store.state.characters;
       },
       loading (){
-        return this.$store.state.characters;
+        return this.$store.state.loading;
       },
       error(){
         return this.$store.state.error;
       }
     },
-    mount(){
+    mounted(){
       this.$store.dispatch('fetchCharacters');
     }
   }
