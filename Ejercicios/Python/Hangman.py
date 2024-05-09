0e0e26b07c9b0fbe80fbf749f1dc60629225af99
Enrique Iranzo Martínez <eiranzo@futurespace.es>
2024-05-09 10:33:59 +0200

    09/05/2024 10:33:58,99

--- Ejercicios/Python/Hangman.py
+++ Ejercicios/Python/Hangman.py
@@ -1,8 +1,10 @@
 #Importaciones necesarias para la lógica
 import csv;
 import random;
-import os;
 from datetime import datetime;
+import uuid;
+import pandas as pd;
+import os;
 
 #Clase principal del programa
 class Hangman:
@@ -18,6 +20,12 @@ class Hangman:
         self.rounds = [];
         #Puntuación
         self.score = 0;
+        #Atributo del ejercicio 3, es un array que va a guardar la info de cada ronda, al final he cambiado alguna cosa y ya no lo uso
+        #self.rounds_info = [];
+        #Atributo del ejercicio 3, el id del juego
+        self.game_id = str(uuid.uuid4());
+        #Hora de inicio
+        self.start_date = datetime.now();
 
     #Método de carga
     def load(self,filename):
@@ -59,8 +67,207 @@ class Hangman:
             #Creo una ronda para cada iteración
             self.round(word);
 
+        #Al final de las rondas mostrar la puntuación
+        print("Tu puntuación es de: "+str(self.score));
+        self.save_game_csv();
+
     #Función para crear una ronda
+    def round(self,word):
+        #Id de la ronda
+        round_id = str(uuid.uuid4());
+        #Array de letras usadas, lo inicializo a vacío
+        guessed = [];
+        #intentos
+        attempts = 0;
+        #Número máximo de intentos
+        max = 5;
+        #Bucle para hacer todos los intentos
+        while attempts < max:
+            #Función que dibuja el ahorcado
+            self.draw_hangman(attempts);
+            #Función que dibuja las letras que quedan
+            self.draw_result(word,guessed);
+            #Dibujar las letras que han sido usadas 
+            if len(guessed) > 0 :
+                print ("Letras usadas: ");
+                for letter in guessed:
+                    print(letter + ", ");
+            #Pedir una letra al usuario
+            user_input = input("Introduce una letra: ").upper();
+            #Si introduce más de una letra, se penaliza
+            if len(user_input) > 1:
+                print("Longitud no válida, se va a aplicar una pequeña penalización :(");
+                attempts += 1;
+                #Si se ha alcanzado el número máximo establezco gameover en true y se acaba el juego
+                if attempts >= max:
+                    self.draw_hangman(attempts);
+                    print ("La palabra era: "+word)
+                    print("Número máximo de intentos alcanzado , ronda perdida");
+                    self.save_round_csv(word,round_id,attempts,False);
+            #Si la letra ya la había usado antes, se penaliza
+            elif user_input in guessed:
+                print("Ya has probado con esta letra, se va a aplicar una penalización :(");
+                attempts += 1;
+                #Si se ha alcanzado el número máximo establezco gameover en true y se acaba el juego
+                if attempts >= max:
+                    self.draw_hangman(attempts);
+                    print ("La palabra era: "+word)
+                    print("Número máximo de intentos alcanzado , ronda perdida");
+                    self.save_round_csv(word,round_id,attempts,False);
+            #Si el usuario ha acertado compruebo si ha sido la última letra a acertar
+            elif user_input in word:
+                #Meto la letra en en array de letras usadas
+                guessed.append(user_input);
+                print("¡Has acertado!");
+                if self.word_guessed(word,guessed):
+                    print("¡Has adivinado la palabra!, un punto para ti");
+                    #Subo la puntuación
+                    self.score += 1;
+                    #Introduzco información en el csv
+                    self.save_round_csv(word,round_id,attempts,True);
+                    #Rompo el condicional
+                    break;
+
+            #Si no es que se ha fallado
+            else:
+                print("Has fallado :(");
+                guessed.append(user_input);
+                #Subo el número de intentos
+                attempts += 1;
+                #Si se ha alcanzado el número máximo establezco gameover en true y se acaba el juego
+                if attempts >= max:
+                    self.draw_hangman(attempts);
+                    print ("La palabra era: "+word)
+                    print("Número máximo de intentos alcanzado , ronda perdida");
+                    self.save_round_csv(word,round_id,attempts,False);
+    
+
+
+
+    def draw_hangman(self,attempts):
+        #Array con las formas, según el número de intentos imprimirá una u otra
+        stages = [
+            """
+                --------
+                |      |
+                |
+                |
+                |
+                |
+                -
+            """,
+            """
+                --------
+                |      |
+                |      O
+                |
+                |
+                |
+                -
+            """,
+            """
+                --------
+                |      |
+                |      O
+                |      |
+                |      |
+                |
+                -
+            """,
+            """
+                --------
+                |      |
+                |      O
+                |     \|/
+                |      |
+                |
+                -
+            """,
+            """
+                --------
+                |      |
+                |      O
+                |     \|/
+                |      |
+                |     /
+                -
+            """,
+            """
+                --------
+                |      |
+                |      O
+                |     \|/
+                |      |
+                |     / \\
+                -
+            """,
+        ];
+        print (stages[attempts]);
+
+    #Función encargada de mostrar las letras que se han introducido correctamente y las _
+    def draw_result(self,word,guess):
+        display = "";
+        for letter in word:
+            if letter in guess:
+                display += letter;
+            else:
+                display += "_";
+        print (display);
+
+    #Función encargada de saber si se ha acertado la palabra del todo
+    def word_guessed(self,word,guessed):
+        for letter in word:
+            if letter not in guessed:
+                return False;
+        
+        return True;
+
+    #Función encargada de guardar los datos del juego en un CSV
+    def save_game_csv(self):
+        end_date = datetime.now();
+        game_frame = pd.DataFrame({
+            "game_id":[self.game_id],
+            "username":[self.username],
+            "start_date":[self.start_date],
+            "end_date":[end_date],
+            "score":[self.score]
+        });
+
+        file_name = "./csv/games.csv";
+
+        #Ver si el archivo está vacío, si lo está escribo el encabezado, si no, escribo el contenido a secas
+        if os.path.exists(file_name):
+            if os.path.getsize(file_name) == 0:
+                game_frame.to_csv(file_name,header=True,index=False);
+            else:
+                game_frame.to_csv(file_name, header=False, index=False,mode="a");
+        else:
+            game_frame.to_csv(file_name,header=True,index=False);
+    #Función encargada de guardar los datos de la ronda en un CSV
+    def save_round_csv(self,word,round_id,attempts,victory):
+        game_frame = pd.DataFrame({
+            "game_id":[self.game_id],
+            "word":[word],
+            "username":[self.username],
+            "round_id":[round_id],
+            "user_try":[attempts],
+            "victory":[victory]
+        });
+
+        file_name = "./csv/rounds_in_games.csv";
+
+        #Ver si el archivo está vacío, si lo está escribo el encabezado, si no, escribo el contenido a secas
+        if os.path.exists(file_name):
+            if os.path.getsize(file_name) == 0:
+                game_frame.to_csv(file_name,header=True,index=False);
+            else:
+                game_frame.to_csv(file_name, header=False, index=False,mode="a");
+        else:
+            game_frame.to_csv(file_name,header=True,index=False);
+        
+
     
 
 #Inicialización del juego
 hangman_game = Hangman();
+hangman_game.start();
